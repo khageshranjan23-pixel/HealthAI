e8df94cc5bdffd410f65b9a3126a17e263743133
Khagesh Ranjan <khageshranjan23@gmail.com>
2026-04-15 02:40:04 +0530

    after rebase

--- Dr.sushamhealthAI/frontend/vite.config.js
+++ Dr.sushamhealthAI/frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:7860',
+        target: 'http://localhost:8000',
         changeOrigin: true,
       }
     }
